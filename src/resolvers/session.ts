85705dda017374b535e591c198a51a9b808d4fcd
Ehsan Tatasadi <ehsan@tatasadi.com>
2023-07-17 11:00:31 +0200

    add comments

--- src/resolvers/session.ts
+++ src/resolvers/session.ts
@@ -1,6 +1,12 @@
 import _ from "lodash"
 
 export default {
+  // this is not a good idea and in this example I did this because
+  // in my database the speakers and sessions are not related to
+  // eachother through _id of the mongodb but through referenceId
+  // the right way to do this, when the data are related through
+  // _id from mongodb is to relate data in prisma and let
+  // prisma include the speakers in sessions and sessions in speakers
   speakers: async (session, args, { dataSources }, info) => {
     const speakers = await dataSources.speakerAPI.getSpeakers()
     return speakers.filter((speaker) => {
--- src/resolvers/speaker.ts
+++ src/resolvers/speaker.ts
@@ -1,6 +1,12 @@
 import _ from "lodash"
 
 export default {
+  // this is not a good idea and in this example I did this because
+  // in my database the speakers and sessions are not related to
+  // eachother through _id of the mongodb but through referenceId
+  // the right way to do this, when the data are related through
+  // _id from mongodb is to relate data in prisma and let
+  // prisma include the speakers in sessions and sessions in speakers
   sessions: async (speaker, args, { dataSources }, info) => {
     const sessions = await dataSources.sessionAPI.getSessions()
     return sessions.filter((session) => {
